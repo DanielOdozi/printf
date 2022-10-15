00eb18505704a40433d479db16b9dbb827b0d078
abdullah <abdallahsuleiman56@gmail.com>
2022-10-15 13:03:05 -0700

    try test file

---
+++ test_files/try.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i;
+
+	va_start (ap, format);
+	i = vfprintf(stdout, format, ap);
+	va_end(ap);
+	return i;
+}
+
+int main(void)
+{
+	char a;
+	a = 'D';
+	char *s;
+	s = "john wayne";
+	int i = 4;
+
+	_printf("My name is\n");
+	_printf("%c\n", a);
+	//two identifiers in one vfprint
+	_printf("%s What is your name good sir?\n %s\n", s);
+	_printf("%d\n", i);
+	_printf("%b\n", 98);
+}
