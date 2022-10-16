311039f31cba4fd72c4fc1d51dd8f858cd3eadd4
Abdullah <abdallahsuleiman56@gmail.com>
2022-10-16 15:01:54 +0100

    trying putchar

--- test_files/try.c
+++ test_files/try.c
@@ -2,29 +2,60 @@
 #include <stdarg.h>
 #include <string.h>
 
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int i;
+        va_list ap;
+        int i = 0,j = 0, len, len2;
 
-	va_start (ap, format);
-	i = vfprintf(stdout, format, ap);
-	va_end(ap);
-	return i;
+         while(format[j])
+        {
+            len= ++j;
+        }
+
+        //checking if it counts properly
+        printf("string length = %d\n",len);
+        j = 0;
+
+        va_start (ap, format);
+        while(format[j])
+        {
+
+                if(format[j] == '%' && format[j+1] == 's')
+                {
+                    int *str = va_arg(ap, int);
+                    while(str[i])
+                    {
+                        _putchar(str[i]);
+                        i++;
+                    }
+                    _putchar('\n');
+                }
+                else if(format[j] == '%' && format[j+1] == 'c')
+                {
+                    int chr = va_arg(ap, int);
+                    _putchar(chr);
+                    _putchar('\n');
+                }
+                else
+                {
+                    _putchar(format[j]);
+                }
+                j++;
+        }
 }
 
 int main(void)
 {
-	char a;
-	a = 'D';
-	char *s;
-	s = "john wayne";
-	int i = 4;
+        char *s;
+        s = "world";
+        char a = 'p';
 
-	_printf("My name is\n");
-	_printf("%c\n", a);
-	//two identifiers in one vfprint
-	_printf("%s What is your name good sir?\n %s\n", s);
-	_printf("%d\n", i);
-	_printf("%b\n", 98);
+        _printf("%s",s);
+        _printf("%c",a);
+        _printf("Howdy");
+
 }
