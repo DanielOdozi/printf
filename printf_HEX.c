1042ec9766fb657de01414e48dd242f994e6fadd
Abdullah <abdallahsuleiman56@gmail.com>
2022-10-18 19:57:08 +0100

    added printf_Hex and the other missing file

--- printf_HEX.c
+++ printf_HEX.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * printf_hex - prints an int in octal form
+ * printf_HEX - prints an int in octal form
  * @heX: int to print
  * @flags: flags
  *
  * Return: length of the number printed
  */
-int printf_hex(va_list heX, char flags[])
+int printf_HEX(va_list heX, char flags[])
 {
 	unsigned long int dig, count = 1, len = 0, temp, x, i;
-	char l = '$';
+	char l;
 
 	for (i = 0; flags[i] != '$'; i++)
 		if (flags[i] == 'h' || flags[i] == 'l')
@@ -23,7 +23,7 @@ int printf_hex(va_list heX, char flags[])
 		dig = va_arg(heX, unsigned int);
 
 	for (i = 0; flags[i] != '$'; i++)
-		len += flag_checker(flags[i], 1, 'h');
+		len += flag_checker(flags[i], 1, 'H');
 
 	temp = dig;
 
@@ -37,27 +37,27 @@ int printf_hex(va_list heX, char flags[])
 		x = dig / count;
 		dig = dig % count;
 		count = count / 16;
-		_putchar(case_l(x));
+		_putchar(case_u(x));
 		len++;
 	}
 	return (len);
 }
 
 /**
- * case_l - determine val fox hexdecima
+ * case_u - determine val fox hexdecima
  * @val: value to use
  * Return: char
  */
-int case_l(unsigned long int val)
+int case_u(unsigned long int val)
 {
 	switch (val)
 	{
-	case 10: return ('a');
-	case 11: return ('b');
-	case 12: return ('c');
-	case 13: return ('d');
-	case 14: return ('e');
-	case 15: return ('f');
+	case 10: return ('A');
+	case 11: return ('B');
+	case 12: return ('C');
+	case 13: return ('D');
+	case 14: return ('E');
+	case 15: return ('F');
 	default: return ('0' + val);
 	}
 }
